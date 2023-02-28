7a455feabbf7845d7238fb5bc7ace13140bca00b
venkat-devish <venkatsuryakiranv@gmail.com>
2023-02-28 18:33:35 +0530

    add: multi select

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
-import SingleSelect from "./components/singleselect/SingleSelect";
+import SingleSelect, {
+  SelectOption,
+} from "./components/singleselect/SingleSelect";
 import { options } from "./data";
 
 function App() {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
   return (
     <div className="App">
       <SingleSelect
+        multiple
         options={options}
-        value={value}
-        onChange={(o) => setValue(o)}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <SingleSelect
+        options={options}
+        value={value2}
+        onChange={(o) => setValue2(o)}
       />
     </div>
   );
--- src/components/singleselect/SingleSelect.tsx
+++ src/components/singleselect/SingleSelect.tsx
@@ -1,27 +1,45 @@
 import { useEffect, useState } from "react";
 import styles from "../../styles/select.module.css";
 
-type SelectOption = {
+export type SelectOption = {
   label: string;
   value: number;
 };
 
-type SingleSelectProps = {
-  options: SelectOption[];
-  value?: SelectOption;
-  onChange: (option: SelectOption | undefined) => void;
+type MultipleSelectProps = {
+  multiple: true;
+  value: SelectOption[];
+  onChange: (value: SelectOption[]) => void;
 };
 
-const SingleSelect = ({ options, value, onChange }: SingleSelectProps) => {
+type SingleSelectProps = {
+  multiple?: false;
+  value?: SelectOption;
+  onChange: (value: SelectOption | undefined) => void;
+};
+
+type SelectProps = {
+  options: SelectOption[];
+} & (SingleSelectProps | MultipleSelectProps);
+
+const SingleSelect = ({ multiple, options, value, onChange }: SelectProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [highlightedOptions, setHighlightedOptions] = useState(0);
 
   function selectedOption(option: SelectOption) {
-    return option === value;
+    return multiple ? value.includes(option) : option === value;
   }
 
   function selectOption(option: SelectOption) {
-    onChange(option);
+    if (multiple) {
+      if (value?.includes(option)) {
+        onChange(value.filter((opt) => opt !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) onChange(option);
+    }
   }
 
   function highlightedOption(idx: number) {
@@ -29,7 +47,7 @@ const SingleSelect = ({ options, value, onChange }: SingleSelectProps) => {
   }
 
   function clearOptions() {
-    onChange(undefined);
+    multiple ? onChange([]) : onChange(undefined);
   }
 
   useEffect(() => {
@@ -47,7 +65,23 @@ const SingleSelect = ({ options, value, onChange }: SingleSelectProps) => {
       tabIndex={0}
       className={styles.container}
     >
-      <div className={styles.value}>{value?.label}</div>
+      <div className={styles.value}>
+        {multiple
+          ? value.map((el) => (
+              <button
+                key={el.value}
+                className={styles["option-badge"]}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(el);
+                }}
+              >
+                {el.label}
+                <span className={styles["remove-button"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </div>
       <button
         onClick={(e) => {
           e.stopPropagation();
